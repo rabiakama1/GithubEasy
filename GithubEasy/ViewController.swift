37b642c4c1b5b31b2ee50748a75f54402aa403dc
Rabia Kama <rabiakamaa@gmail.com>
2025-08-18 18:48:59 +0300

    project created

---
+++ GithubEasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubEasy
+//
+//  Created by Şükrü Hasta on 18.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubEasy/presentation/home/cell/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  GithubEasy
+//
+//  Created by Şükrü Hasta on 18.08.2025.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
